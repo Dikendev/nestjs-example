5fc0df3686f2ce5ae00fa9339f546466099b5815
Dikendev <diken.dev@gmail.com>
2023-06-05 02:18:48 -0300

    app module temp

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,30 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Global, Module } from '@nestjs/common';
+import { EntryService } from './entry/entry.service';
+import { EntryModule } from './entry/entry.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { EntryService } from './entry/entry/entry.service';
+import { DataSource } from 'typeorm';
+import { Entry } from './entry/entry.entity';
+import { EntryController } from './entry/entry.controller';
 
+@Global()
 @Module({
-  imports: [TypeOrmModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService, EntryService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: 'localhost',
+      port: 3306,
+      username: 'nest',
+      password: 'nest',
+      database: 'nestexample',
+      entities: [Entry],
+      synchronize: true,
+    }),
+    EntryModule,
+  ],
+  controllers: [ EntryController],
+  providers: [EntryService,],
+  exports: [EntryService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
