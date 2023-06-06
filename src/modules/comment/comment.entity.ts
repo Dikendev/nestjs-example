ae305ee253bb858791de2f89285416d1442fbe72
Dikendev <diken.dev@gmail.com>
2023-06-05 23:56:36 -0300

    comment entity

--- src/modules/comment/comment.entity.ts
+++ src/modules/comment/comment.entity.ts
@@ -2,10 +2,12 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
   VersionColumn,
 } from 'typeorm';
+import { Entry } from '../entry/entry.entity';
 
 @Entity()
 export class Comment {
@@ -14,12 +16,19 @@ export class Comment {
 
   @Column('text')
   body: string;
+
   @Column('simple-json')
   author: { first_name: string; last_name: string };
+
+  @ManyToOne((type) => Entry, (entry) => entry.comments)
+  entry: Entry;
+
   @CreateDateColumn()
   created_at: Date;
+
   @UpdateDateColumn()
   modified_at: Date;
+
   @VersionColumn()
   revision: number;
 }
